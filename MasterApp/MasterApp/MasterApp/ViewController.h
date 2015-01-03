e0d26797ec6a122e771bfcdf36cfe6ea5eeffd53
Jon Carroll <jon@orbotix.com>
2015-01-03 16:36:32 -0700

    First commit of master app

---
+++ MasterApp/MasterApp/MasterApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MasterApp
+//
+//  Created by Jon Carroll on 1/3/15.
+//  Copyright (c) 2015 Orbotix, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
