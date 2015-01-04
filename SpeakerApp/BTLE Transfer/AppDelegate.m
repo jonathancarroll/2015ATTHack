60a97532561d6e455acca12ba70ddc6f52c92f30
Brandon Dorris <brandon@orbotix.com>
2015-01-04 08:54:03 -0800

    disabled idle timer on speaker app

--- SpeakerApp/BTLE Transfer/AppDelegate.m
+++ SpeakerApp/BTLE Transfer/AppDelegate.m
@@ -56,6 +56,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    application.idleTimerDisabled = YES;
     return YES;
 }
 							
